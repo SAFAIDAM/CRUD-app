a70af760ca16ddfb7b6779c201afc902a5c9ff8e
SAFA IDAM HAMED <116453067+SAFAIDAM@users.noreply.github.com>
2024-01-01 18:46:10 +0100

    *

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: 'https://crud-app-front-ten.vercel.app',
-      // ['https://crud-app-front-ten.vercel.app',
-      // 'https://crud-app-front-ten.vercel.app/add',
-      // 'https://crud-app-front-ten.vercel.app/createUser',
-      // 'https://crud-app-front-ten.vercel.app/updateUser/:id',
-      // 'https://crud-app-front-ten.vercel.app/deleteUser/:id']
+    origin: '*',
+    // ['https://crud-app-front-ten.vercel.app',
+    // 'https://crud-app-front-ten.vercel.app/add',
+    // 'https://crud-app-front-ten.vercel.app/createUser',
+    // 'https://crud-app-front-ten.vercel.app/updateUser/:id',
+    // 'https://crud-app-front-ten.vercel.app/deleteUser/:id']
     methods: ['POST', 'DELETE', 'GET', 'PUT'],
     credentials: true,
   })
